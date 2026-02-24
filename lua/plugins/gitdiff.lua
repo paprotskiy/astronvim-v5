4237b70cb5349a9fdb92fafb315c81893110c0b8
Roman Paprotskiy <paprotskiy.roman@gmail.com>
2026-02-24 15:26:06 +0100

    update diffview config

--- lua/plugins/gitdiff.lua
+++ lua/plugins/gitdiff.lua
@@ -1,41 +1,65 @@
+local period = 500
+local stop_autorefresh = nil
+
 return {
   "sindrets/diffview.nvim",
   event = "User AstroGitFile",
-  cmd = { "DiffviewOpen" },
+  cmd = { "DiffviewOpen", "DiffviewAutorefresh" },
   opts = {
     enhanced_diff_hl = false,
     view = {
       default = { winbar_info = true },
       file_history = { winbar_info = false },
     },
-    hooks = { diff_buf_read = function(bufnr) vim.b[bufnr].view_activated = true end },
+    hooks = {
+      diff_buf_read = function(bufnr)
+        vim.b[bufnr].view_activated = true
+        vim.bo[bufnr].readonly = true
+      end,
+      view_closed = function()
+        if stop_autorefresh then
+          stop_autorefresh()
+          stop_autorefresh = nil
+        end
+      end,
+    },
   },
   config = function(_, opts)
     require("diffview").setup(opts)
-    local watcher = vim.uv.new_fs_event()
-    watcher:start(vim.uv.cwd(), { recursive = true }, function(err)
-      if err then return end
-      vim.schedule(function()
-        if #require("diffview.lib").views > 0 then vim.cmd "DiffviewRefresh" end
-      end)
-    end)
 
-    local timer = vim.uv.new_timer()
-    timer:start(500, 500, function()
-      vim.schedule(function()
-        if #require("diffview.lib").views > 0 then
-          vim.cmd "DiffviewRefresh"
-          vim.cmd "checktime"
-        end
-      end)
-    end)
+    vim.api.nvim_create_user_command("DiffviewAutorefresh", function(args)
+      vim.cmd("DiffviewOpen " .. args.args)
 
-    local function refresh_diffview()
-      if #require("diffview.lib").views > 0 then vim.cmd "DiffviewRefresh" end
-    end
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufAdd", "FocusGained" }, {
-      callback = refresh_diffview,
-    })
+      local watcher = vim.uv.new_fs_event()
+      watcher:start(vim.uv.cwd(), { recursive = true }, function(err)
+        if err then return end
+        vim.schedule(function()
+          if vim.fn.getcmdwintype() == "" then vim.cmd "DiffviewRefresh" end
+        end)
+      end)
+
+      local timer = vim.uv.new_timer()
+      timer:start(period, period, function()
+        vim.schedule(function()
+          if vim.fn.getcmdwintype() == "" then
+            vim.cmd "DiffviewRefresh"
+            vim.cmd "checktime"
+          end
+        end)
+      end)
+
+      local autocmd_id = vim.api.nvim_create_autocmd({ "BufWritePost", "BufAdd", "FocusGained" }, {
+        callback = function()
+          if vim.fn.getcmdwintype() == "" then vim.cmd "DiffviewRefresh" end
+        end,
+      })
+
+      stop_autorefresh = function()
+        watcher:close()
+        timer:close()
+        vim.api.nvim_del_autocmd(autocmd_id)
+      end
+    end, { nargs = "*" })
   end,
   specs = {
     {
